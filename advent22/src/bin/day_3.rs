b8ec1ffdef6792e45feccd1e8a6aec4be649019f
juimin <d95wang@gmail.com>
2022-12-04 01:25:31 -0800

    finish day 3 part 2

--- advent22/src/bin/day_3.rs
+++ advent22/src/bin/day_3.rs
@@ -12,9 +12,13 @@ fn populate_compartment_mapping(line: &[u8], lower: usize, upper: usize, compart
 }
 
 fn main() {
-    let file_contents = get_input_contents();
+    let file_contents: String = get_input_contents();
 
-    let mut priority_sum = 0;
+    let mut priority_sum: usize = 0;
+    let mut grouped_priority_sum: usize = 0;
+
+    let mut group_member = 1;
+    let mut group_badge_mapping: [i32; 52] = [0; 52];
 
     for line in file_contents.split("\n") {
         if line.len() == 0 {
@@ -37,7 +41,29 @@ fn main() {
                 priority_sum += i + 1;
             }
         }
+
+        let mut full_sack_contents: [i32; 52] = [0; 52];
+        populate_compartment_mapping(line_bytes, 0, line_bytes.len(), &mut full_sack_contents);
+
+        for i in 0..52 {
+            if full_sack_contents[i] > 0 && group_badge_mapping[i] < group_member {
+                group_badge_mapping[i] += 1;
+            }
+        }
+
+        if group_member == 3 {
+            for i in 0..52 {
+                if group_badge_mapping[i] == 3 {
+                    grouped_priority_sum += i + 1;
+                }
+            }
+            group_badge_mapping = [0; 52];
+            group_member = 1;
+        } else {
+            group_member += 1;
+        }
     }
 
-    println!("Sum of priorities for part 1: {}", priority_sum);
+    println!("Sum of priorities: {}", priority_sum);
+    println!("Sum of grouped badge priorities: {}", grouped_priority_sum);
 }
