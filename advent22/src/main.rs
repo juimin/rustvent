4b31f42de3f704f239c2f7f689fb1593601f07b5
juimin <d95wang@gmail.com>
2022-12-03 20:36:36 -0800

    add clap cli

--- advent22/src/main.rs
+++ advent22/src/main.rs
@@ -1,3 +1,20 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    day: i16,
+
+    #[arg(short, long)]
+    input_file: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!(
+        "Running advent22 - day {} on input {}",
+        args.day, args.input_file
+    );
 }
