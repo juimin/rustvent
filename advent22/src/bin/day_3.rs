2e10a01e9b02686b527e3b047376a0206ad67a6b
juimin <d95wang@gmail.com>
2022-12-04 01:04:33 -0800

    cleanup

--- advent22/src/bin/day_3.rs
+++ advent22/src/bin/day_3.rs
@@ -1,5 +1,16 @@
 use advent22::get_input_contents;
 
+fn populate_compartment_mapping(line: &[u8], lower: usize, upper: usize, compartment: &mut [i32]) {
+    for i in lower..upper {
+        let c = line[i] as char as usize;
+        if c >= 65 && c <= 90 {
+            compartment[c - 65 + 26] += 1;
+        } else if c >= 97 && c <= 122 {
+            compartment[c - 97] += 1;
+        }
+    }
+}
+
 fn main() {
     let file_contents = get_input_contents();
 
@@ -13,26 +24,13 @@ fn main() {
         let mut compartment_one: [i32; 52] = [0; 52];
         let mut compartment_two: [i32; 52] = [0; 52];
 
-        for i in 0..line.len() / 2 {
-            let c = line_bytes[i] as char as usize;
-            if c >= 65 && c <= 90 {
-                compartment_one[c - 65 + 26] += 1;
-            } else if c >= 97 && c <= 122 {
-                compartment_one[c - 97] += 1;
-            }
-        }
-
-        for i in line.len() / 2..line.len() {
-            let c = line_bytes[i] as char as usize;
-            if c >= 65 && c <= 90 {
-                compartment_two[c - 65 + 26] += 1;
-            } else if c >= 97 && c <= 122 {
-                compartment_two[c - 97] += 1;
-            }
-        }
-
-        // println!("{:?}", compartment_one);
-        // println!("{:?}", compartment_two);
+        populate_compartment_mapping(line_bytes, 0, line_bytes.len() / 2, &mut compartment_one);
+        populate_compartment_mapping(
+            line_bytes,
+            line_bytes.len() / 2,
+            line_bytes.len(),
+            &mut compartment_two,
+        );
 
         for i in 0..52 {
             if compartment_one[i] > 0 && compartment_two[i] > 0 {
