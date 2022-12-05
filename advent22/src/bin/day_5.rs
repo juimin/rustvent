e30949970c796ed783c92de5fbd514f3d27f0f1c
juimin <d95wang@gmail.com>
2022-12-05 00:58:31 -0800

    day 5 part 1 done
    fuck this input

--- advent22/src/bin/day_5.rs
+++ advent22/src/bin/day_5.rs
@@ -1,4 +1,74 @@
 use advent22::get_input_contents;
+
+fn parse_initial_state(lines: &String) -> Vec<Vec<char>> {
+    let lines_split: Vec<&str> = lines.split("\n").collect();
+    let stack_count = (lines_split[0].len() + 1) / 4;
+
+    let mut initial_state: Vec<Vec<char>> = Vec::new();
+
+    for _ in 0..stack_count {
+        initial_state.push(Vec::new());
+    }
+
+    for line in lines_split {
+        if line.len() == 0 {
+            continue;
+        }
+        let line_bytes = line.as_bytes();
+        for i in 0..stack_count {
+            let c = line_bytes[1 + (4 * i)] as char;
+            if c.is_alphabetic() {
+                initial_state[i].push(c);
+            }
+        }
+    }
+
+    for index in 0..initial_state.len() {
+        initial_state[index].reverse();
+    }
+
+    return initial_state;
+}
+
+fn parse_moves(state: &mut Vec<Vec<char>>, input: &String) {
+    for line in input.split("\n") {
+        if !line.starts_with("move") {
+            continue;
+        }
+
+        let tokens: Vec<&str> = line.split_whitespace().collect();
+        let qnt = tokens[1].parse::<usize>().expect("qnt should be numeric");
+        let source = tokens[3]
+            .parse::<usize>()
+            .expect("source should be numeric")
+            - 1;
+        let dest = tokens[5].parse::<usize>().expect("dest should be numeric") - 1;
+
+        for _ in 0..qnt {
+            if state[source].len() > 0 {
+                let c = state[source].pop().expect("something is here");
+                state[dest].push(c);
+            }
+        }
+    }
+}
+
 fn main() {
     let file_contents = get_input_contents();
+
+    let initial_state_string = file_contents
+        .split("move")
+        .next()
+        .expect("expected an initial state before moves")
+        .to_string();
+
+    let mut state = parse_initial_state(&initial_state_string);
+
+    parse_moves(&mut state, &file_contents);
+
+    for stack in state {
+        if stack.len() > 0 {
+            print!("{}", stack.last().expect("something should be there"));
+        }
+    }
 }
