7cf98e37413956d2d43aa6fed44ad1f2c3e99eb6
juimin <d95wang@gmail.com>
2022-12-05 22:59:01 -0800

    clean up 6

--- advent22/src/bin/day_6.rs
+++ advent22/src/bin/day_6.rs
@@ -1,17 +1,17 @@
 use advent22::get_input_contents;
 use std::collections::HashSet;
 
-fn detect_start_of_marker(s: &str, start_idx: usize, marker_size: i32) -> i32 {
-    let mut idx: usize = start_idx;
+fn detect_start_of_marker(s: &str, marker_size: usize) -> i32 {
+    let mut idx: usize = 0;
     let mut character_set: HashSet<char> = HashSet::new();
     let s_bytes = s.as_bytes();
 
-    while idx < s.len() - marker_size as usize {
+    while idx < s.len() - marker_size {
         for i in 0..marker_size {
             character_set.insert(s_bytes[idx + i as usize] as char);
         }
         if character_set.len() == marker_size as usize {
-            return idx as i32 + marker_size;
+            return (idx + marker_size) as i32;
         } else {
             character_set.clear();
             idx += 1;
@@ -22,12 +22,12 @@ fn detect_start_of_marker(s: &str, start_idx: usize, marker_size: i32) -> i32 {
 }
 
 fn main() {
-    let file_contents = get_input_contents();
+    let file_contents: String = get_input_contents();
 
     for line in file_contents.trim().split("\n") {
-        let start_of_packet = detect_start_of_marker(&line, 0, 4);
+        let start_of_packet: i32 = detect_start_of_marker(&line, 4);
         println!("Start of packet: {}", start_of_packet);
-        let start_of_message = detect_start_of_marker(&line, 0, 14);
+        let start_of_message: i32 = detect_start_of_marker(&line, 14);
         println!("Start of message: {}", start_of_message);
     }
 }
